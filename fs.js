8dc193b33cf7bc5e813063f9cdbbd412ccd066ce
iki1uc <entertrainer@web.de>
2026-09-14 07:20:08 +0200

    Update fs.js

--- fs.js
+++ fs.js
@@ -1,5 +1,5 @@
-// OPFS-Layer für QUEAST - persistent, auch nach Reload da
-class QueastFS {
+// OPFS-Layer für FREEBOOTE - persistent, auch nach Reload
+export class QueastFS {
   async init() {
     this.root = await navigator.storage.getDirectory();
   }
@@ -12,10 +12,9 @@ class QueastFS {
   async read(path) {
     const fileHandle = await this.root.getFileHandle(path);
     const file = await fileHandle.getFile();
-    return await file.arrayBuffer(); // perfekt für deine hdf.rom Module
+    return await file.arrayBuffer();
   }
   async mountRAM() {
-    // Dein tmpfs - simuliert Knoppix RAM-FS
-    this.ramFS = new Map(); // später durch SharedArrayBuffer ersetzen
+    this.ramFS = new Map(); // dein tmpfs wie bei Knoppix
   }
 }
